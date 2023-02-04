45ed1b058cc72b249bb89ba4de30d428bc1ac801
Muhammad Rizki <muhammadrizkitkj123@gmail.com>
2023-02-04 20:32:49 +0800

    fix bug

--- application/views/info_dosen.php
+++ application/views/info_dosen.php
@@ -82,7 +82,6 @@
 
             <nav class="navbar col-lg-12 col-12 p-lg-0 fixed-top d-flex flex-row">
                 <div class="navbar-menu-wrapper d-flex align-items-stretch justify-content-between">
-                    <a class="navbar-brand brand-logo-mini align-self-center d-lg-none" href="index.html"><img src="assets/images/logo-mini.svg" alt="logo" /></a>
                     <button class="navbar-toggler navbar-toggler align-self-center mr-2" type="button" data-toggle="minimize">
                         <i class="mdi mdi-menu"></i>
                     </button>
